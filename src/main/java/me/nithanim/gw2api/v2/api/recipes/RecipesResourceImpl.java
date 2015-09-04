b7101449b65e753263127e84309348b13e5ab410
Nithanim <nithanim@yahoo.de>
2015-09-04 07:55:55 +0200

    Added recipe search

--- src/main/java/me/nithanim/gw2api/v2/api/recipes/RecipesResourceImpl.java
+++ src/main/java/me/nithanim/gw2api/v2/api/recipes/RecipesResourceImpl.java
@@ -2,9 +2,23 @@ package me.nithanim.gw2api.v2.api.recipes;
 
 import com.sun.jersey.api.client.WebResource;
 import me.nithanim.gw2api.v2.util.rest.IdsResourceBase;
+import me.nithanim.gw2api.v2.util.rest.RequestHelper;
 
 public class RecipesResourceImpl extends IdsResourceBase<Recipe, int[]> implements RecipesResource {
+    private final WebResource searchWebResource;
+    
     public RecipesResourceImpl(WebResource webResource) {
         super(webResource, "recipes", Recipe.class, int[].class);
+        searchWebResource = this.webResource.path("search");
+    }
+
+    @Override
+    public int[] searchByInput(int id) {
+        return RequestHelper.getRequest(searchWebResource, int[].class, "input", String.valueOf(id));
+    }
+
+    @Override
+    public int[] searchByOutput(int id) {
+        return RequestHelper.getRequest(searchWebResource, int[].class, "output", String.valueOf(id));
     }
 }
