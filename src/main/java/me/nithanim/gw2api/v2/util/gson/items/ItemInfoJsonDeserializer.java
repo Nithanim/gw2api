8e7ceb802111a603e3881c01ce57d441c7688b31
Nithanim <nithanim@yahoo.de>
2015-10-30 15:50:39 +0100

    Fix id in ItemInfo always 0

--- src/main/java/me/nithanim/gw2api/v2/util/gson/items/ItemInfoJsonDeserializer.java
+++ src/main/java/me/nithanim/gw2api/v2/util/gson/items/ItemInfoJsonDeserializer.java
@@ -14,39 +14,24 @@ import me.nithanim.gw2api.v2.api.items.ItemInfo;
 import me.nithanim.gw2api.v2.api.items.ItemType;
 
 public class ItemInfoJsonDeserializer implements JsonDeserializer<ItemInfo> {
-    private static final Map<String, Field> fields = new HashMap<>();
-    
-    static {
-        Field[] fs = ItemInfo.class.getDeclaredFields();
-        for(Field f : fs) {
-            f.setAccessible(true);
-            String name = toJsonName(f.getName());
-            fields.put(name, f);
+    private final Map<String, Field> fields = new HashMap<>();
+
+    public ItemInfoJsonDeserializer() {
+        Class<?> clazz = ItemInfo.class;
+        while (clazz != null) {
+            registerAllFieldsFor(clazz);
+            clazz = clazz.getSuperclass();
         }
     }
 
-    private static String toJsonName(String name) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(name);
-        for(int i = 0; i < sb.length(); i++) {
-            char c = sb.charAt(i);
-            if(Character.isUpperCase(c)) {
-                sb.setCharAt(i, '_');
-                sb.insert(i+1, Character.toLowerCase(c));
-            }
-        }
-        return sb.toString();
-    }
-    
-    
     @Override
     public ItemInfo deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
         JsonObject root = json.getAsJsonObject();
         ItemInfo ii = new ItemInfo();
-        for(Map.Entry<String, JsonElement> t : root.entrySet()) {
-            if(!t.getKey().equals("details")) {
+        for (Map.Entry<String, JsonElement> t : root.entrySet()) {
+            if (!t.getKey().equals("details")) {
                 Field field = fields.get(t.getKey());
-                if(field != null) {
+                if (field != null) {
                     Object value = context.deserialize(t.getValue(), field.getType());
                     try {
                         field.set(ii, value);
@@ -56,10 +41,10 @@ public class ItemInfoJsonDeserializer implements JsonDeserializer<ItemInfo> {
                 }
             }
         }
-        
+
         ItemType type = ii.getType();
         Class<? extends Details> detailClass = type.getDetailsClass();
-        if(detailClass != null) {
+        if (detailClass != null) {
             Object details = context.deserialize(root.get("details"), detailClass);
             try {
                 Field field = fields.get("details");
@@ -69,5 +54,34 @@ public class ItemInfoJsonDeserializer implements JsonDeserializer<ItemInfo> {
             }
         }
         return ii;
+    }
+
+    private void registerAllFieldsFor(Class clazz) {
+        Field[] fs = clazz.getDeclaredFields();
+        for (Field f : fs) {
+            f.setAccessible(true);
+            String name = toJsonName(f.getName());
+
+            Field prev = fields.put(name, f);
+            if (prev != null) {
+                throw new IllegalStateException(
+                    "Another field with the name "
+                    + f.getName() + " was already found in "
+                    + prev.getDeclaringClass() + " before reading " + clazz);
+            }
+        }
+    }
+
+    private static String toJsonName(String name) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(name);
+        for (int i = 0; i < sb.length(); i++) {
+            char c = sb.charAt(i);
+            if (Character.isUpperCase(c)) {
+                sb.setCharAt(i, '_');
+                sb.insert(i + 1, Character.toLowerCase(c));
+            }
+        }
+        return sb.toString();
     }
 }
