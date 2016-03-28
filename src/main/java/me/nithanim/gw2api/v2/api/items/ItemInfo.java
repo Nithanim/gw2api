4a4f1d3f53788ef181ad382aa34947eea779f593
Nithanim <nithanim@yahoo.de>
2016-03-28 18:10:27 +0200

    Added javadoc for instructions on how to use/get the details of an item specific to its type.

--- src/main/java/me/nithanim/gw2api/v2/api/items/ItemInfo.java
+++ src/main/java/me/nithanim/gw2api/v2/api/items/ItemInfo.java
@@ -68,6 +68,26 @@ public class ItemInfo extends BasicItem {
         return restrictions;
     }
 
+    /**
+     * Returns an object containing additional information specific to the
+     * {@link ItemType}. The resulting {@link Details} of this method has to be
+     * casted manually to the specialized subclass.
+     * <p>
+     * All the available subclasses can be found in the subpackage "details".
+     * The names of the classes start with the type and ends with "Details". As
+     * an example, {@link ItemType.ARMOR} relates to
+     * {@link me.nithanim.gw2api.v2.api.items.details.ArmorDetails}. You might
+     * want to use your IDE to list all possible subclasses of {@link Details}.
+     * </p>
+     * <p>
+     * As a sidenote: {@link ItemType#getDetailsClass()} returns the specific
+     * subclass of {@link Details} for a specific {@link ItemType} if you ever
+     * need that for some reason.
+     *
+     * @return the {@link Details} object that contains all additional
+     * information for its {@link ItemType}. Needs to be casted manually to the
+     * specific subclass according to the {@link ItemType}.
+     */
     public Details getDetails() {
         return details;
     }
