32adef1ab952e7e470e72cae4e62638bfa55fc10
Nithanim <nithanim@yahoo.de>
2016-06-30 14:45:45 +0200

    Added new global Account values

--- src/main/java/me/nithanim/gw2api/v2/api/account/Account.java
+++ src/main/java/me/nithanim/gw2api/v2/api/account/Account.java
@@ -13,4 +13,13 @@ public class Account {
     private int world = -1;
     private String[] guilds;
     private DateTime created;
+    private Access access;
+    private int fractalLevel = -1;
+    private int dailyAp;
+    private int monthlyAp;
+    private int wvwRank;
+
+    public enum Access {
+        NONE, PLAY_FOR_FREE, GUILD_WARS_2, HEART_OF_THORNS;
+    }
 }
