76f7cab16a10f805420caba19e901a9d5346db46
Nithanim <git@nithanim.me>
2019-01-21 02:11:36 +0100

    Fixed #22 missing id in recipe ingredients.

--- src/main/java/me/nithanim/gw2api/v2/common/BasicItem.java
+++ src/main/java/me/nithanim/gw2api/v2/common/BasicItem.java
@@ -1,11 +1,14 @@
 package me.nithanim.gw2api.v2.common;
 
+import com.google.gson.annotations.SerializedName;
+
 @lombok.NoArgsConstructor
 @lombok.AllArgsConstructor
 @lombok.Getter
 @lombok.EqualsAndHashCode
 @lombok.ToString
 public class BasicItem {
+    @SerializedName(value = "id", alternate = "item_id")
     private int id = -1;
     private int count = 1;
 }
