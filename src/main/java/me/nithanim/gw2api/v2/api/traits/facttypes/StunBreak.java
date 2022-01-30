a43e96a030806354a21ae34b61c532ed0feaacaa
Nithanim <git@nithanim.me>
2022-01-30 14:08:46 +0100

    Add missing fact stun break

---
+++ src/main/java/me/nithanim/gw2api/v2/api/traits/facttypes/StunBreak.java
@@ -0,0 +1,11 @@
+package me.nithanim.gw2api.v2.api.traits.facttypes;
+
+import me.nithanim.gw2api.v2.api.traits.FactBase;
+
+@lombok.NoArgsConstructor
+@lombok.Getter
+@lombok.EqualsAndHashCode(callSuper = true)
+@lombok.ToString
+public class StunBreak extends FactBase {
+  private boolean value;
+}
